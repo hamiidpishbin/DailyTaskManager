3a1385714697a9c3fc4d07a2a2e364ee74a3237b
Hamid Pishbin <hamidpishbin89@gmail.com>
2024-01-08 00:14:06 +0330

    Fix typos in agent, again.

--- DailyTaskManager.Web/client-app/src/api/agent.js
+++ DailyTaskManager.Web/client-app/src/api/agent.js
@@ -6,13 +6,13 @@ const responseBody = (response) => response.data;
 
 const requests = {
   get: (url) => axios.get(url).then(responseBody),
-  post: (url, body) => axios.post < T > (url, body).then(responseBody),
+  post: (url, body) => axios.post(url, body).then(responseBody),
   put: (url, body) => axios.put(url, body).then(responseBody),
   delete: (url) => axios.delete(url).then(responseBody),
 };
 
 const Account = {
-  current: () => requests.get < User > "/account",
+  current: () => requests.get("/account"),
   login: (user) => requests.post("/account/login", user),
   register: (user) => requests.post("/account/register", user),
 };
